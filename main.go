8c0c2325efbcbb681b80ceacd9195f796ee3f272
nimish <nimcurry@yahoo.co.in>
2024-12-25 16:53:48 -0500

    created one project from section 6

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
+)
+
+func main() {
+	title, content := getNoteData()
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	userNote.Display()
+	err = userNote.Save()
+
+	if err != nil {
+		fmt.Println("saving the note failed...")
+		return
+	}
+
+	fmt.Println("saving the note succeeded")
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title: ")
+
+	content := getUserInput("Note content: ")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+	//var value string
+	//fmt.Scan(&value)
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
